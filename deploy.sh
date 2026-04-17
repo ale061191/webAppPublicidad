225413aaa387d68be268b79ed9186d8470c02330
Voltaje Plus <dev@voltaje.plus>
2026-04-17 16:58:08 -0400

    refactor: Clean up unused files - remove Convex, deployment scripts, and temporary files

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Rename problematic files before deploy
-find . -name "mutations*" -type f ! -path "./convex/*" -delete
-find . -name "queries*" -type f ! -path "./convex/*" -delete
-npx convex deploy --yes
